d3619d97ac0d5c5efddd5cd9b21efc84845be70b
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-16 17:25:48 +0800

    finished js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inArray, ...toRemove) {
+    for (let i = 0; i < inArray.length; i++){
+        for (let j = 0; j <  toRemove.length; j ++){
+            if (inArray[i] === toRemove[j]){
+                inArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return inArray;
 };
 
 // Do not edit below this line
