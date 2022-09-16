c00401d148939b557410f1f6dd7bbc33e64aaf0e
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-16 17:03:35 +0800

    finished js exercies 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(theString, reps) {
+    let outString = '';
+    if (reps >= 0) {
+        for (let i = 0; i < reps; i++) {
+            outString = outString + theString;
+        }
+    }else {
+        outString = 'ERROR';
+    }
+    return outString;
 };
 
 // Do not edit below this line
