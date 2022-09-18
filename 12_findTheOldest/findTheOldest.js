1ee73f3ff84cf5720dc336295a6c3e8e2e028b22
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-18 19:06:19 +0800

    Finished js exercise 12
    The test expecting the object rather than the name threw me for a loop

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest, age = 0, oldestAge = 0;
+    const d = new Date();
+    for (let i = 0; i < people.length; i++) {
+        if (!people[i].yearOfDeath) {
+            age = d.getFullYear() - people[i].yearOfBirth;
+        }else {
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
 
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
