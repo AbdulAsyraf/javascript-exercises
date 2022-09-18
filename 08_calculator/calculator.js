978315dcdfd5beb7ac30dc9b6c58cfd563285bc1
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-18 18:26:14 +0800

    Finished js exercise 8
    Realized the multi input functions are not actually unknow number of
    input arguments but an array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum += num[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let product = 1;
+  for (let i = 0; i < num.length; i++) {
+    product *= num[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	return Math.pow(num, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {
+    return 1;
+  }else {
+    // return num * factorial(num - 1);
+    return multiply([num, factorial(num - 1)]);
+  }
 };
 
 // Do not edit below this line
