9dd1a1fb18e4f4bd87c7fb7e0106c8fda797cc53
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-18 18:03:41 +0800

    Finished js exercise 7
    Js exercise 7 is the temperature conversion program

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(inTemp) {
+  let outTemp = Math.round(((inTemp - 32) * 5 / 9) * 10) / 10;
+  return outTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(inTemp) {
+  let outTemp = Math.round(((inTemp * 9 / 5) + 32) * 10) / 10;
+  return outTemp;
 };
 
 // Do not edit below this line
