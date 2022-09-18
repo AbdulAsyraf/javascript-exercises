a86639e5d3c9c87b045e434cfe5e4fdc26bb66cc
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-18 18:42:23 +0800

    Finished js exercise 9
    Used regex in js for the first time

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (theString) {
+    let regExp = /[^a-z]/gi;
+    let cleanString = theString.replace(regExp, '').toUpperCase();
+    let reverseString = cleanString.split('').reverse().join('');
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
